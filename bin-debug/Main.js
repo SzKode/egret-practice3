9528f1fa7e5454f940ff8ce25b49d8ff3cd0962d
Joker <keerzhou@qq.com>
2018-10-31 15:06:54 +0800

    commit

--- bin-debug/Main.js
+++ bin-debug/Main.js
@@ -14,7 +14,69 @@ var Main = (function (_super) {
         return _this;
     }
     Main.prototype.onAddToStage = function (event) {
-        RES.addEventListener();
+        // 初始化Resource资源加载库
+        RES.addEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.loadConfig("resource/default.res.json", "resource/");
+    };
+    /**
+     * 配置文件加载OK，开始预加载preload资源组
+     */
+    Main.prototype.onConfigComplete = function (event) {
+        RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+        RES.loadGroup("preload");
+    };
+    /**
+     * 加载资源组出错
+     */
+    Main.prototype.onResourceLoadError = function (event) {
+        console.warn("Group" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        this.onResourceLoadComplete(event);
+    };
+    /**
+     * 资源组加载进度
+     */
+    Main.prototype.onResourceProgress = function (event) {
+        if (event.groupName == "preload") { }
+    };
+    /**
+     * 资源组加载出错
+     */
+    Main.prototype.onItemLoadError = function (event) {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    };
+    /**
+     * preload资源组加载完成
+     */
+    Main.prototype.onResourceLoadComplete = function (event) {
+        if (event.groupName == "preload") {
+            RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createScene();
+        }
+    };
+    /**
+     * Create Scene
+     */
+    Main.prototype.createScene = function () {
+        var bg = this.createBitmapByName("bg_jpg");
+        this.addChild(bg);
+        var normalTree = this.createBitmapByName("normalTree_png");
+        this.addChild(normalTree);
+        normalTree.y = this.height / 2 - normalTree.height / 2;
+        normalTree.x = this.width / 2 - normalTree.width / 2;
+    };
+    Main.prototype.createBitmapByName = function (name) {
+        var result = new egret.Bitmap();
+        var texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
     };
     return Main;
 }(egret.DisplayObjectContainer));
--- src/Main.ts
+++ src/Main.ts
@@ -17,15 +17,64 @@ class Main extends egret.DisplayObjectContainer {
     private onConfigComplete(event: RES.ResourceEvent): void {
         RES.removeEventListener(RES.ResourceEvent.CONFIG_COMPLETE, this.onConfigComplete, this);
         RES.addEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+        RES.addEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+        RES.addEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
         RES.loadGroup("preload");
     }
 
     /**
-     * 
+     * 加载资源组出错
+     */
+    private onResourceLoadError(event: RES.ResourceEvent): void {
+        console.warn("Group" + event.groupName + " has failed to load");
+        //忽略加载失败的项目
+        this.onResourceLoadComplete(event);
+    }
+
+    /**
+     * 资源组加载进度
+     */
+    private onResourceProgress(event: RES.ResourceEvent): void {
+        if(event.groupName == "preload"){}
+    }
+
+    /**
+     * 资源组加载出错
+     */
+    private onItemLoadError(event: RES.ResourceEvent): void {
+        console.warn("Url:" + event.resItem.url + " has failed to load");
+    }
+
+    /**
+     * preload资源组加载完成
      */
     private onResourceLoadComplete(event: RES.ResourceEvent): void {
         if(event.groupName == "preload"){
             RES.removeEventListener(RES.ResourceEvent.GROUP_COMPLETE, this.onResourceLoadComplete, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_LOAD_ERROR, this.onResourceLoadError, this);
+            RES.removeEventListener(RES.ResourceEvent.GROUP_PROGRESS, this.onResourceProgress, this);
+            RES.removeEventListener(RES.ResourceEvent.ITEM_LOAD_ERROR, this.onItemLoadError, this);
+            this.createScene();
         }
+    }
+
+    /**
+     * Create Scene
+     */
+    private createScene(): void {
+        var bg: egret.Bitmap = this.createBitmapByName("bg_jpg");
+        this.addChild(bg);
+        var normalTree: egret.Bitmap = this.createBitmapByName("normalTree_png");
+        this.addChild(normalTree);
+        normalTree.y = this.height/2 - normalTree.height/2;
+        normalTree.x = this.width/2 - normalTree.width/2;
+    }
+
+    private createBitmapByName(name: string): egret.Bitmap {
+        var result: egret.Bitmap = new egret.Bitmap();
+        var texture: egret.Texture = RES.getRes(name);
+        result.texture = texture;
+        return result;
     }
 }
